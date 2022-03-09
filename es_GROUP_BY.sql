fa98816aef09647d38858515fcc086482ace1b36
Narresx <92748159+Narresx@users.noreply.github.com>
2022-03-09 18:07:32 +0100

    add second SQL string ( group by )

--- es_GROUP_BY.sql
+++ es_GROUP_BY.sql
@@ -8,7 +8,15 @@
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
 
-SELECT COUNT(`id`) AS 'n_students',YEAR(`enrolment_date`) AS 'year' FROM `students`  
+SELECT COUNT(`id`) AS 'n_students',YEAR(`enrolment_date`) AS 'year' 
+FROM `students`  
 GROUP BY YEAR(`enrolment_date`);
+
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT COUNT(`id`) AS 'n_teachers',`office_address` AS 'address' 
+FROM `teachers`  
+GROUP BY `office_address`;
 
 
