80b32d38a2ba19c0f025e890941d4c871a3934f8
Narresx <92748159+Narresx@users.noreply.github.com>
2022-03-11 10:40:00 +0100

    add bonus JOIN

--- es_JOIN.sql
+++ es_JOIN.sql
@@ -89,3 +89,16 @@ WHERE `departments`.`name`= 'Dipartimento di Matematica';
 
 
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+
+SELECT `students`.`name`, `students`.`surname`, `courses`.`name`, 
+COUNT(`exam_student`.`vote`) AS 'numero_tentativi', 
+MAX(`exam_student`.`vote`) AS 'voto_massimo'
+FROM `students`
+JOIN `exam_student`
+ON `exam_student`.`id` = `students`.`id`
+JOIN `exams`
+ON `exams`.`id` = `exam_student`.`exam_id`
+JOIN `courses`
+ON `courses`.`id` = `exams`.`course_id`
+GROUP BY `students`.`id`, `courses`.`id`
+HAVING `voto_massimo` >= 18;
